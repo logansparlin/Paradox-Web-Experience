6ecb9b892d64ba45fd41c281afab8237139d2f0d
logansparlin <logan.sparlin@gmail.com>
2015-04-13 21:26:35 -0500

    changes

--- client/views/home.js
+++ client/views/home.js
@@ -2,24 +2,41 @@ Template.home.rendered = function() {
 	$(window).scroll(function(e) {
 		e.preventDefault();
 	})
+	var index = 0,
+		section = $('section'),
+		pager = $('.slide-navigation ul li');
+
+	Session.set('index', index)
+
+	Tracker.autorun(function() {
+		var data = Session.get('index');
+		pager.eq(index).addClass('active')
+	})
+
 	// Previous and Next
 	var detectDirection = function(event, delta) {
 		event.preventDefault();
-		console.log($(event.target))
 		// var delta = event.originalEvent.delta;
 		if(delta < 0) {
 			// next
 			$('body').removeClass('previous')
 			$('body').addClass('next')
-			$('.landing').addClass('hidden')
-			console.log('next')
+			$('section').removeClass('hidden')
+			$('section').eq(index).addClass('hidden')
+
+
+			pager.eq(index).removeClass('active')
+			index = index += 1;
+			Session.set('index', index)
 		}
 		else if(delta > 0) {
 			// previous
 			$('body').removeClass('next')
 			$('body').addClass('previous')
-			$('.landing').removeClass('hidden')
-			console.log('previous')
+			section.removeClass('hidden')
+			pager.eq(index).removeClass('active')
+			index = index -= 1;
+			Session.set('index', index)
 		}
 	}
 	var changeData = _.debounce(detectDirection, 50, true);
